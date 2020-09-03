2fc17219b576786c50ed7d28cb090e5804c8b567
njerimk <nmurugikamau@gmail.com>
2020-09-03 15:42:57 -0400

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -6,20 +6,9 @@ class User
     def initialize
         @first_name = first_name
         @last_name = last_name
-        @knowledge = []
     end
 
-    def learn(knowledge)
-        @knowledge << knowledge
-    end
-    
-    def knowledge
-        @knowledge
-    end
 
-    def teach
-        @knowledge
-    end
 
 
 
